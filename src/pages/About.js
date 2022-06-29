6d223206d56105c507cc11e212e5dc7831044234
Brandon Jones <bdjones15@crimson.ua.edu>
2022-06-29 06:48:45 +0100

    Week 1 Submission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic_name from "../assets/YearbookPhoto.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
+        <div>
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src = {profile_pic_name}
               alt="Profile Pic"
-            ></img>
+              width="500" 
+              height="600"
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+            <div className="name_title">Brandon Jones</div>
+            <div className="brief_description">Hello my name is Brandon Jones and I am a CS major at the University of Alabama. I watch stand up comedy and Naruto is undisputably the greatest show to ever grace the eyes of man.</div>
           </div>
         </div>
       </div>
-    );
+
+      </div>
+    )
   }
 }
